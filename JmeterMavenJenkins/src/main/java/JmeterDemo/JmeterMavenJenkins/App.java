793001b990221a3f9af9e37b1e5c58d820536ffe
yewuxue <37978945+yewuxue@users.noreply.github.com>
2018-04-01 23:15:35 +0800

    接口自动化

---
+++ JmeterMavenJenkins/src/main/java/JmeterDemo/JmeterMavenJenkins/App.java
@@ -0,0 +1,13 @@
+package JmeterDemo.JmeterMavenJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
